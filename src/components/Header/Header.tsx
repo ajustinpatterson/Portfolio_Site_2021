06cea5f5134903b2a46220febb83579cf0b6806f
Justin Patterson <PJ943@yahoo.com>
2020-11-17 18:00:53 +0100

    init commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <button className="nav-btn">
+        <img src="../../assets/logo192.png" alt="placeholder" />
+      </button>
+      <button className="nav-btn">
+        <img src="../../assets/logo192.png" alt="placeholder" />
+      </button>
+      <button className="nav-btn">
+        <img src="../../assets/logo192.png" alt="placeholder" />
+      </button>
+    </div>
+  );
+};
+
+export default Header;
