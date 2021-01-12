a0dbae5f1910f6a85e9a1f624c0e87a819262ea6
Justin Patterson <PJ943@yahoo.com>
2021-01-12 13:12:49 +0100

    feat: add Portfolio container; flesh out structure for displaying projects

---
+++ src/containers/Portfolio/Portfolio.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Portfolio.scss';
+
+const Portfolio = () => {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="project"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
