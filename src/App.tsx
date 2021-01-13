125844c92ab95b7c1869f479b1061c98879254d7
Justin Patterson <PJ943@yahoo.com>
2021-01-13 14:56:52 +0100

    feat(components): add footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import FrontPage from './containers/FrontPage/FrontPage';
 import Header from './components/Header/Header';
 import Portfolio from './containers/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <FrontPage />
       <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        Technologies used on this page include React, Typescript, SASS, and
+        email.js
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
