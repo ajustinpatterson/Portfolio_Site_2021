92eb8fa125d6ce7cb16d63e19c31b479103e1e7b
Justin Patterson <PJ943@yahoo.com>
2021-01-19 13:58:39 +0100

    chore(about): chore commit before lunch

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -25,7 +25,17 @@ const About = () => {
         </p>
         <p>
           Now, I'm working on more front end
-          <Link to="/Portfolio">challenges</Link>
+          <Link to="/Portfolio">challenges</Link> and sharpening my skills on
+          custom react applications while dipping into back end technologies
+          like Golang and Java.
+        </p>
+        <p>
+          In my free time, I'm known to walk around Barcelona and snap photos
+          with my old Fuji X-10 for my super old
+          <a href="http://cancionencantada.tumblr.com">travel blog</a>. I'm also
+          a wannabe novelist and avid cyclist. Please
+          <a href="mailto:ajustinpatterson@outlook.com">get in touch</a> if I
+          can be of any service to you.
         </p>
       </div>
     </motion.div>
