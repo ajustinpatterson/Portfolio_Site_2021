33064a33bac2bc72071b80d7a5affef0924d21fe
Justin Patterson <PJ943@yahoo.com>
2021-01-19 12:42:45 +0100

    feat(add framermotion; re-do app.tsx, index.tsx): add fr
    amermotion; re-do app.tsx, index.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+import { Switch, Route, useLocation, useHistory } from 'react-router-dom';
 
 import Nav from './components/Nav/Nav';
 import FrontPage from './containers/FrontPage/FrontPage';
@@ -16,21 +17,33 @@ const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
 
 function App() {
+  const [isFirstMount, setIsFirstMount] = React.useState(true);
+  const location = useLocation();
+  const history = useHistory();
+
+  React.useEffect(() => {
+    const unlisten = history.listen(() => {
+      isFirstMount && setIsFirstMount(false);
+    });
+
+    return unlisten;
+  }, [history, isFirstMount]);
+
   return (
-    <Router>
-      <div className="App" id="outer-container">
-        <Nav outerContainerId={'outer-container'} pageWrapId={'page-wrap'} />
-        <Header />
-        <div id="page-wrap">
+    <div className="App" id="outer-container">
+      <Nav outerContainerId={'outer-container'} pageWrapId={'page-wrap'} />
+      <Header />
+      <div id="page-wrap">
+        <AnimatePresence>
           <Switch>
             <Route exact path="/" component={FrontPage} />
             <Route path="/portfolio" component={Portfolio} />
             <Route path="/contact" component={Contact} />
           </Switch>
-        </div>
-        <Footer />
+        </AnimatePresence>
       </div>
-    </Router>
+      <Footer />
+    </div>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const history = createBrowserHistory();
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router {...{ history }}>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
