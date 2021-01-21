ad3358c2d2d5abe96101d775a0c7e1b6928cac2e
Justin Patterson <PJ943@yahoo.com>
2021-01-21 12:41:01 +0100

    feat(about): add links/styling to About container

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -38,11 +38,42 @@ const About = () => {
           like Golang and Java.
         </p>
         <p>
+          I've done some different jobs in my life, including a long stint in
+          the service industry, some basic freelance SEO, running a modest
+          <a
+            href="https://pj9434.wixsite.com/alan-j-patterson"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {' '}
+            wedding photography business
+          </a>
+          , and teaching English abroad.
+        </p>
+        <p>
+          With backgrounds in education, high-pressure work environments, and
+          being a one-man business operation, I'm committed to writing the most
+          elegant, easily-maintainable code possible, and am capable of
+          communicating handily both on a technical and product level. But my
+          greatest strength is team work.
+        </p>
+        <p>
           In my free time, I'm known to walk around Barcelona and snap photos
           with my old Fuji X-10 for my super old
-          <a href="http://cancionencantada.tumblr.com"> travel blog</a>. I'm
-          also a wannabe novelist and an avid fan of vintage bicycles. Please do
-          not hesitate to
+          <a
+            href="http://cancionencantada.tumblr.com"
+            rel="noreferrer"
+            target="_blank"
+          >
+            {' '}
+            travel/photo blog
+          </a>
+          . I'm also a wannabe novelist and an avid fan of vintage bicycles
+          (what can I say? It turns out, I'm just kind of a fan of fixing
+          things).
+        </p>
+        <p>
+          Please do not hesitate to
           <a href="mailto:ajustinpatterson@outlook.com"> get in touch</a> if I
           can be of any service to you.
         </p>
